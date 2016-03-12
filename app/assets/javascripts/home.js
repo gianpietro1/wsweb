2725139acd6f0c97e2f5a7becb030e48b9d7ee89
Gianpietro Lavado <gianpietro1@gmail.com>
2016-03-12 11:24:58 -0500

    dynami menus and styling

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,5 +1,13 @@
 $(document).ready(function() {
     $('#main-menu li:first').addClass('current');
-    $('#nav').onePageNav();
+    $('#nav').onePageNav({
+    begin: function() {
+        //Hack so you can click other menu items after the initial click
+        $('body').append('<div id="device-dummy" style="height: 1px;"></div>');
+    },
+    end: function() {
+        $('#device-dummy').remove();
+    }
+});
 });
 
